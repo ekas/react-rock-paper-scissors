2b5979d8decc854443402f48c95068df4eb9fad4
Ekas <ekaspreet93.singh@gmail.com>
2022-08-30 17:46:42 +0200

    update: move router provider to updare app

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import Landing from "./components/Landing";
+import { useEffect, useState } from "react";
+import { GameMode } from "./types/Game";
+
 import "./App.less";
 
 function App() {
-  <Router>
+  const navigate = useNavigate();
+  const [gameMode, setGameMode] = useState<GameMode>();
+
+  useEffect(() => {
+    gameMode && navigate("/game");
+  }, [gameMode]);
+
+  return (
     <div className="App">
       <main className="container">
         <Routes>
-          <Route path="/" element={<></>} />
+          <Route path="/">
+            <Route path="" element={<Landing setGameMode={setGameMode} />} />
+          </Route>
         </Routes>
       </main>
     </div>
-  </Router>;
+  );
 }
 
 export default App;
